7ed56c968e1bfb2a02fbd4cdb883a6a7c19d3306
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-03-17 16:45:31 -0500

    Add files via upload

--- app_api/models/db.config.js
+++ app_api/models/db.config.js
@@ -1,1 +1,3 @@
-
+module.exports = {
+    url: 'mongodb://localhost:27017/movie'
+}
---
+++ app_api/models/db.model.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/movie';
+mongoose.connect(dbURI);
+
+// Configuring the database
+
+
+mongoose.Promise = global.Promise;
+
+
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+});
+
+var MovieSchema = mongoose.Schema({
+    Username: String,
+    Password: String,
+    Movies: {
+        Titles: String
+    }
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
